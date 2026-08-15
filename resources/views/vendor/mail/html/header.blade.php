90fe2d9b41d649361e52df43be2e9358c45ed953
Davi Alves <davi.wordpress@gmail.com>
2026-08-15 14:29:27 +0100

    refactor: update domain names from painel.jrspice.com to painel.xpiceconnections.com

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://painel.jrspice.com/logo-black.png" alt="{{ config('app.name') }}" width="200" style="display: block; width: 200px; height: auto; border: 0;">
+<img src="https://painel.xpiceconnections.com/logo-black.png" alt="{{ config('app.name') }}" width="200" style="display: block; width: 200px; height: auto; border: 0;">
 </a>
 </td>
 </tr>
